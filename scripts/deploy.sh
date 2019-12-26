b7174dd2751bd3b990c965e0cb14bd07c1d4741c
Linlin Yan <yanlinlin82@gmail.com>
2019-12-27 00:19:21 +0800

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-hugo && rsync -avP --delete public/ yanlinlin.cn:/var/www/blog/ \
-	--exclude ARTS-Weekly/ \
-	--exclude ARTS-Weekly-BioMed/
+hugo && rsync -avP --delete public/ yanlinlin.cn:/var/www/blog/
